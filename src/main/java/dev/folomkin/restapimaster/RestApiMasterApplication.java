13e2b169e571ecc95c3524cefe57b729b11ca99c
Dmitriy Folomkin <dmitriy.folomkin@yandex.ru>
2025-11-20 16:47:05 +0300

    init app

---
+++ src/main/java/dev/folomkin/restapimaster/RestApiMasterApplication.java
@@ -0,0 +1,13 @@
+package dev.folomkin.restapimaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiMasterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestApiMasterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/folomkin/restapimaster/RestApiMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.folomkin.restapimaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiMasterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
